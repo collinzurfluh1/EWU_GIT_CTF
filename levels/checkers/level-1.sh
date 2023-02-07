fbd0fa71a624461ea5854d34d69d7a9a9d8c0063
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-02-07 11:45:58 -0800

    test level1

--- levels/checkers/level-1.sh
+++ levels/checkers/level-1.sh
@@ -15,6 +15,7 @@ popd
 
 # We know that there's only one commit in the changes - otherwise it would have failed before.
 number_of_files_changed=$( git diff --stat $old $new | grep "files changed" | awk ' {print $1} ' )
-if [[ $number_of_files_changed -ne 1 ]]
+echo $number_of_files_changed;
+if [[ $number_of_files_changed -ne 2 ]]
     then reject-solution "More than 1 file was changed! Only add computerScienceRocks.txt. Check out the original branch and try again.";
 fi
