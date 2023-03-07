412da0ee40d7d6ef1e2ec0220c30157b28eb34a6
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-07 15:13:28 -0800

    level-8 test

---
+++ levels/checkers/level-8.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source $(dirname $0)/checkers-lib.sh
+
+read old new ref < /dev/stdin
+
+dump_dir=$(dump-commit-to-directory $new)
+
+pushd $dump_dir
+    if [ ! -f runme.py ];
+        then reject-solution "coolscript.py is missing.";
+    fi
+
+    echo "Trying to execute the script ./coolscript.py..."
+    echo
+
+    # Will raise if not merged
+    ./coolscript.py
+popd
+
+# Check that merge was fast forward merge.
+new_commit=$(get_commit_of $new)
+target_commit=$(get_commit_of mergehelper1-tag)
+if [ "$new_commit" != "$target_commit" ]; then
+    reject-solution "Merge should have been a fast-forward merge. Try again."
+fi
