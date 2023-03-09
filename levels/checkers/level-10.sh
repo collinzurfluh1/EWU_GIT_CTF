b29813ce468e36bf1a87080527bc8290266877c9
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-09 12:44:58 -0800

    Added check

--- levels/checkers/level-10.sh
+++ levels/checkers/level-10.sh
@@ -21,9 +21,11 @@ pushd $dump_dir
 
 popd
 
-how_many_parents=$(how_many_parents $new)
-if [ $how_many_parents -ne 3 ]; then
-    reject-solution "You must not have merged and done a merge revert.."
+commit_amount=$( git log level-2..$new --oneline | wc -l )
+echo $commit_amount
+
+if [ $commit_amount -ne 2 ];
+    then reject-solution "Seems like you didnt merge and then do a merge revert.";
 fi
 
 log_n=5
