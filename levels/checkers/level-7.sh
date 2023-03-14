0713a218c56c725191f70790b79c55acaddcd499
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-13 20:06:35 -0700

    added level-7

--- levels/checkers/level-7.sh
+++ levels/checkers/level-7.sh
@@ -7,15 +7,15 @@ read old new ref < /dev/stdin
 dump_dir=$(dump-commit-to-directory $new)
 
 pushd $dump_dir
-    if [ ! -f coolscript.py ];
-        then reject-solution "coolscript.py is missing.";
+    if [ ! -f mergerscript.py ];
+        then reject-solution "mergerscript.py is missing.";
     fi
 
 popd
 
 # Check that merge was fast forward merge.
 new_commit=$(get_commit_of $new)
-target_commit=$(get_commit_of mergehelper1-tag)
+target_commit=$(get_commit_of mergehelper4-tag)
 if [ "$new_commit" != "$target_commit" ]; then
     reject-solution "Merge should have been a fast-forward merge. Try again."
 fi
